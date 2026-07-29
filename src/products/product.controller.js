ea1c30a02239ec4917487ad7c1feaec7edcfb5bb
kaioumdev <afrankaiyum34@gmail.com>
2026-07-29 21:11:01 +0600

    feat(getAllProducts): make getAllProducts funtion to get all products

--- src/products/product.controller.js
+++ src/products/product.controller.js
@@ -25,3 +25,42 @@ const createNewProduct = async (req, res) => {
         return errorResponse(res, 500, "Failed to create new product", error)
     }
 }
+
+onst getAllProducts = async (req, res) => {
+
+    try {
+        const {category, color, minPrice, maxPrice, page=1, limit=10} = req.query;
+        const filter = {};
+        if(category && category !== 'all') {
+            filter.category = category;
+        }
+        if(color && color !== 'all') {
+            filter.color = color;
+        }
+
+        if(minPrice && maxPrice) {
+            const min = parseFloat(minPrice);
+            const max = parseFloat(maxPrice);
+            if(!isNaN(min) && !isNaN(max)) {
+                filter.price = {$gte: min, $lte: max}
+            }
+        }
+        const skip = (parseInt(page) - 1) * parseInt(limit);
+        const totalProducts =  await Products.countDocuments(filter);
+        const totalPages = Math.ceil(totalProducts / parseInt(limit))
+        const products = await Products.find(filter)
+            .skip(skip)
+            .limit(parseInt(limit))
+            .populate('author', 'email username')
+            .sort({createdAt: -1})
+            ;
+
+        return successResponse(res, 200, "Products fetched successfully", data={
+            products,
+            totalProducts,
+            totalPages
+        })
+    } catch (error) {
+        return errorResponse(res, 500, "Failed to get all products", error)
+    }
+}
